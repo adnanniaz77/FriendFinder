1d3f71785d1958a0d6dbbff679603ac0df2cfc81
Adnan Niaz <adnanniaz77@yahoo.com>
2020-01-04 03:35:02 -0800

    all done

--- app/data/friends.js
+++ app/data/friends.js
@@ -46,6 +46,36 @@ const friends = [
         photo:
             "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+        name: "Shane",
+        photo: "https://i.pravatar.cc/150?img=22",
+        scores: ["1", "2", "5", "3", "5", "3", "1", "5", "3", "2"]
+    },
+    {
+        name: "Jennifer",
+        photo: "https://i.pravatar.cc/150?img=53",
+        scores: ["1", "1", "4", "4", "1", "4", "5", "3", "2", "2"]
+    },
+    {
+        name: "Mario",
+        photo: "https://i.pravatar.cc/150?img=93",
+        scores: ["1", "1", "1", "2", "3", "2", "2", "5", "4", "2"]
+    },
+    {
+        name: "Azevado",
+        photo: "https://i.pravatar.cc/150?img=26",
+        scores: ["3", "3", "4", "3", "5", "1", "1", "5", "2", "5"]
+    },
+    {
+        name: "Timmy",
+        photo: "https://i.pravatar.cc/150?img=3",
+        scores: ["2", "1", "4", "4", "3", "1", "2", "1", "4", "1"]
+    },
+    {
+        name: "Clearance",
+        photo: "https://i.pravatar.cc/150?img=6",
+        scores: ["4", "1", "5", "1", "3", "1", "2", "1", "3", "2"]
     }
 ];
 
